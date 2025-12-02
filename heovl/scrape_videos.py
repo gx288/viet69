92c51917d1c1c03013c57b93add38380868415d5
Gxxx <rappernx@gmail.com>
2025-12-02 11:19:32 +0700

    Update scrape_videos.py

--- heovl/scrape_videos.py
+++ heovl/scrape_videos.py
@@ -50,8 +50,16 @@ SHEET_ID = config['SHEET_ID']
 os.makedirs(DATA_FOLDER, exist_ok=True)
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0 Safari/537.36',
-    'Accept-Language': 'vi-VN,vi;q=0.9',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'vi-VN,vi;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Cache-Control': 'max-age=0',
 }
 
 # ====================== GLOBAL DATA ======================
